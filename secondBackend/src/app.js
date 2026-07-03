e3a927a44b73f90295d113eb3c89277409983123
Nilam-Kumbhar <nilamkumbhar1605@gmail.com>
2026-07-03 20:02:02 +0530

    backend setup

--- secondBackend/src/app.js
+++ secondBackend/src/app.js
@@ -18,5 +18,12 @@ app.use(express.static("public")) //this used to store img, febicon,pdf data
 app.use(cookieParser())
 
 
+// routes import
+
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
 
 export {app}
